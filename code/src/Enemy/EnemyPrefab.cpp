05b2da1d4cf026cfa50067ee97560c15bf7c4cf5
Ronan Vlak <ronanvlak@gmail.com>
2024-12-04 10:30:09 +0100

    updated animations

--- code/src/Enemy/EnemyPrefab.cpp
+++ code/src/Enemy/EnemyPrefab.cpp
@@ -17,11 +17,11 @@ SpriteDef firstFrameEnemyIdle = {
     Rect{enemyIdlePosition.x, enemyIdlePosition.y, enemySpriteWidth, enemySpriteHeight},
     enemySpriteWidth, enemySpriteHeight};
 
-const Point enemyWalkingPosition = {22, 389};
+const Point enemyWalkingPosition = {16, 388};
 SpriteDef firstFrameEnemyWalking = {
     "Dungeontileset/0x72_DungeonTilesetII_v1.7.png",
-    Rect{enemyWalkingPosition.x, enemyWalkingPosition.y, enemySpriteWidth, enemySpriteHeight},
-    enemySpriteWidth, enemySpriteHeight};
+    Rect{enemyWalkingPosition.x, enemyWalkingPosition.y, 32, 28},
+    32, 28};
 
 EnemyPrefab::EnemyPrefab() {}
 
@@ -46,6 +46,7 @@ void EnemyPrefab::addSprite(GameObject* gameObject) {
     EngineBravo& engine = EngineBravo::getInstance();
     Sprite* sprite = engine.getResourceManager().createSprite(firstFrameEnemyIdle);
     sprite->setLayer(3);
+    sprite->setTag("idleSprite");
     gameObject->addComponent(sprite);
 }
 
@@ -67,43 +68,26 @@ void EnemyPrefab::addCollider(GameObject* gameObject) {
 void EnemyPrefab::addAnimations(GameObject* gameObject) {
     EngineBravo& engine = EngineBravo::getInstance();
 
-    Animation* enemyIdleAnimation = nullptr;
     Animation* enemyWalkingAnimation = nullptr;
 
-    {
-        std::vector<SpriteDef> enemyIdleAnimationFrames =
-            SpriteDefUtil::extrapolateSpriteDef(firstFrameEnemyIdle, 4);
 
-        std::vector<SpriteDef> enemyAnimationIdle = {
-            enemyIdleAnimationFrames[0], enemyIdleAnimationFrames[1], enemyIdleAnimationFrames[2],
-            enemyIdleAnimationFrames[3]};
-
-        enemyIdleAnimation =
-            engine.getResourceManager().loadAnimation(enemyAnimationIdle, 100, true);
-    }
 
     {
         std::vector<SpriteDef> enemyWalkingAnimationFrames =
-            SpriteDefUtil::extrapolateSpriteDef(firstFrameEnemyWalking, 4);
+            SpriteDefUtil::extrapolateSpriteDef(firstFrameEnemyWalking, 8);
 
-        std::vector<SpriteDef> enemyAnimationWalking = {
-            enemyWalkingAnimationFrames[0], enemyWalkingAnimationFrames[1], enemyWalkingAnimationFrames[2],
-            enemyWalkingAnimationFrames[3]};
 
         enemyWalkingAnimation =
-            engine.getResourceManager().loadAnimation(enemyAnimationWalking, 100, true);
+            engine.getResourceManager().loadAnimation(enemyWalkingAnimationFrames, 400, true);
     }
 
-    enemyIdleAnimation->setTag("enemyIdle");
     enemyWalkingAnimation->setTag("enemyWalking");
 
-    gameObject->addComponent(enemyIdleAnimation);
     gameObject->addComponent(enemyWalkingAnimation);
 
     for (auto animation : gameObject->getComponents<Animation>()) {
         animation->setActive(false);
         animation->setLayer(2);
     }
-
-    enemyIdleAnimation->setActive(true);
+    enemyWalkingAnimation->setActive(false);
 }
