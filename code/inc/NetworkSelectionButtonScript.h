25bab9f5900b09ce99fd930fa50b4747049be91c
Siempie30 <siemvermeulen@hotmail.com>
2024-12-11 10:57:34 +0100

    Removed NetworkSelectionButtonScript class, because it was no longer used

--- code/inc/NetworkSelectionButtonScript.h
+++ code/inc/NetworkSelectionButtonScript.h
@@ -5,34 +5,20 @@
 
 #include "Text.h"
 
-class NetworkSelectionButtonScript : public IButtonBehaviourScript {
+class ConnectButtonScript : public IButtonBehaviourScript
+{
 public:
-    NetworkSelectionButtonScript();
-    ~NetworkSelectionButtonScript();
+	ConnectButtonScript(Text* aTextObject);
+	~ConnectButtonScript();
 
-    std::unique_ptr<Component> clone() const override { return std::make_unique<NetworkSelectionButtonScript>(*this); }
+	std::unique_ptr<Component> clone() const override { return std::make_unique<ConnectButtonScript>(*this); }
 
-    void onButtonPressed() override;
+	void onButtonPressed() override;
 
-    void onButtonReleased() override;
+	void onButtonReleased() override;
 
 private:
-    void setButtonsVisibility();
-};
-
-class ConnectButtonScript : public IButtonBehaviourScript {
-public:
-    ConnectButtonScript(Text* aTextObject);
-    ~ConnectButtonScript();
-
-    std::unique_ptr<Component> clone() const override { return std::make_unique<ConnectButtonScript>(*this); }
-
-    void onButtonPressed() override;
-
-    void onButtonReleased() override;
-
-private:
-    Text* mTextObject{nullptr};
+	Text* mTextObject{nullptr};
 };
 
 #endif // NETWORKBUTTONSCRIPT_H
