ec5e5f5eaee997bc932ccf5dd32d530d6217d079
Seger01 <sjw.sars@student.avans.nl>
2025-08-18 16:30:00 +0200

    Removed all existing code but kept the resources, now a simple scene with dino sprite shown

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,46 +4,66 @@
 #include "Components/ParticleEmitter.h"
 #include "Components/RigidBody.h"
 #include "Input/Input.h"
-#include "SplashScreenBehaviourScript.h"
 #include <Engine/EngineBravo.h>
 #include <Engine/SceneManager.h>
 #include <GameObject.h>
 #include <Scene.h>
 
+#include "SpriteDef.h"
+
+SpriteDef spriteDef = {"DinoSprites.png", Rect(4, 3, 15, 17), 15, 17};
+
+class PlayerBehaviourScript : public IBehaviourScript
+{
+public:
+    void onStart() override
+    {
+        mGameObject->setTag("Player");
+
+        Sprite* playerSprite = EngineBravo::getInstance().getResourceManager().createSprite(spriteDef);
+
+        mGameObject->addComponent(playerSprite);
+    }
+
+    void onUpdate() override {}
+
+    void onCollide(GameObject* aGameObject) override {}
+
+    std::unique_ptr<Component> clone() const override { return std::make_unique<IBehaviourScript>(*this); };
+};
+
 void init()
 {
-	EngineBravo::getInstance().initialize();
-	SceneManager& sceneManager = EngineBravo::getInstance().getSceneManager();
+    EngineBravo::getInstance().initialize();
+    EngineBravo::getInstance().getRenderSystem().setBackgroundColor(Color(0xe6, 0x19, 0xd3));
+    SceneManager& sceneManager = EngineBravo::getInstance().getSceneManager();
 
-	Scene& scene = sceneManager.createScene("SplashScreenScene");
-	GameObject* initObject = new GameObject;
-	initObject->addComponent<SplashScreenBehaviourScript>();
-	initObject->setTag("initObject");
+    Scene& scene = sceneManager.createScene("only_scene");
 
-	GameObject* temp = new GameObject;
-	temp->setTag("temp");
+    Camera* camera = new Camera;
+    camera->setTag("MainCamera");
+    camera->setActive(true);
 
-	// scene->addGameObject(temp);
-	scene.addGameObject(initObject);
-	Camera* camera = new Camera;
-	camera->setTag("MainCamera");
-	camera->setActive(true);
+    camera->setTransform(Transform(Vector2(0, 0)));
+    camera->setWidth(16 * 20);
+    camera->setHeight(9 * 20);
 
-	camera->setTransform(Transform(Vector2(100, 100)));
-	camera->setWidth(200);
-	camera->setHeight(200);
+    camera->setBackgroundColor(Color(0, 0, 0));
 
-	scene.addGameObject(camera);
+    scene.addGameObject(camera);
 
-	sceneManager.requestSceneChange("SplashScreenScene");
+    GameObject* initObject = new GameObject;
+    initObject->addComponent<PlayerBehaviourScript>();
+    scene.addGameObject(initObject);
 
-	EngineBravo::getInstance().run();
+    sceneManager.requestSceneChange("only_scene");
 
-	return;
+    EngineBravo::getInstance().run();
+    return;
 }
 
 int main()
 {
-	init();
-	return 0;
+    init();
+    return 0;
 }
